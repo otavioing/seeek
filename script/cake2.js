b582ec26e999633c02f10885166ec2fe4b77aeef
otavioing <162048765+otavioing@users.noreply.github.com>
2024-09-12 17:34:47 -0300

    Add files via upload

---
+++ script/cake2.js
@@ -0,0 +1,13 @@
+function botaofavo(){
+
+    document.getElementById('coracao').style.display = "none";
+    document.getElementById('coracao2').style.display = "inline";
+
+}
+
+function botaofavo2(){
+
+    document.getElementById('coracao').style.display = "inline";
+    document.getElementById('coracao2').style.display = "none";
+
+}
