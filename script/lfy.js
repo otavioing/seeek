24eb019cde7d60b9bf930a94525741b6d73aacc6
Joao Felipe Vicente <161844290+lfywho@users.noreply.github.com>
2024-09-13 12:34:21 -0300

    Add files via upload

--- script/lfy.js
+++ script/lfy.js
@@ -8,11 +8,11 @@ function paginacarregada() {
     botaomenu.addEventListener("click", abrirMenu);
 
     function abrirMenu() {
-        if(menu.style.right == "-100%" || menu.style.right == "") {
-            menu.style.right = "0";
+        if(menu.style.left == "-100%" || menu.style.left == "") {
+            menu.style.left = "0";
         } else {
-           if(menu.style.right = "0" || menu.style.right == "") {
-            menu.style.right = "-100%";
+           if(menu.style.left = "0" || menu.style.left == "") {
+            menu.style.left = "-100%";
            }
         }
         
