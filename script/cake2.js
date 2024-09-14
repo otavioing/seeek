6232bc118e2c497670e2cc84290ce18a2a580e0e
otavioing <162048765+otavioing@users.noreply.github.com>
2024-09-14 10:07:26 -0300

    Add files via upload

--- script/cake2.js
+++ script/cake2.js
@@ -41,3 +41,20 @@ function f() {
    }
 }
 
+const btnScrollTop = document.querySelector(".btn-scroll-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    btnScrollTop.classList.add("show-btn-scroll-top");
+  } else {
+    btnScrollTop.classList.remove("show-btn-scroll-top");
+  }
+});
+
+btnScrollTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
