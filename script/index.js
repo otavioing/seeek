1268667e75e5f931beba1b1f9832e99a3cd5d1ab
BFugimoto <brunofujimoto98@gmail.com>
2024-10-24 17:03:24 -0300

    Re-re-implementação do modal (Incompleta)

--- script/index.js
+++ script/index.js
@@ -1,16 +1,17 @@
 // Script para a janela modal
 var modal = document.getElementById("modalPort");
 var imgs = document.querySelectorAll(".portImg");
+var divPort = document.querySelectorAll(".divPortifolio");
 var modalImage = document.getElementById("modalImage");
 var span = document.getElementsByClassName("close")[0];
 var body = document.body;
 
-for (let i = 0; i < imgs.length; i++) {
-    imgs[i].onclick = function() {
+for (let i = 0; i < divPort.length; i++) {
+    divPort[i].addEventListener("click", () => {
         modalImage.src = this.src;
         modal.style.display = "block";
         body.style.overflow = "hidden";
-    }
+    });
 }
 
 span.onclick = function() {
