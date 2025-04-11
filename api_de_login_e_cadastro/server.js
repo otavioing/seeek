cca8b20aab14471b7322601f5a5619c19bbeb6cc
otavioing <otaviodominguessilva@gmail.com>
2025-04-11 20:10:15 -0300

    bug resolvido

--- api_de_login_e_cadastro/server.js
+++ api_de_login_e_cadastro/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static('../uploads'));
 
 
 
--- api_de_login_e_cadastro/src/config/upload.js
+++ api_de_login_e_cadastro/src/config/upload.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // pasta onde salvará as imagens
+    cb(null, '../uploads'); // pasta onde salvará as imagens
   },
   filename: (req, file, cb) => {
     const uniqueName = Date.now() + '-' + file.originalname;
