17ddfddcd53f14cb24572464e05524cd4e96d8c6
otavioing <otaviodominguessilva@gmail.com>
2025-04-27 09:47:25 -0300

    tema troca ao fazer o login

--- script/user_logado.js
+++ script/user_logado.js
@@ -1,12 +1,14 @@
-    const usuario = JSON.parse(localStorage.getItem("usuarioLogado"));
+const usuario = JSON.parse(localStorage.getItem("usuarioLogado"));
 
-    if (usuario) {
-      document.getElementById("usersite").innerHTML = `<p id="nome_user">${usuario.nome}</p>
+if (usuario) {
+  document.getElementById(
+    "usersite"
+  ).innerHTML = `<p id="nome_user">${usuario.nome}</p>
       <button onclick="abrirMenu()">
         <img id="img_user" src="http://localhost:4500${usuario.foto}" alt="User" />
       </button>`;
 
-      document.getElementById("menuperfil").innerHTML = `<div class="divmenuperfil">
+  document.getElementById("menuperfil").innerHTML = `<div class="divmenuperfil">
         <a href="usuario.html" class="Perf">Meu Perfil</a>
       </div>
        <div class="divmenuperfil">
@@ -21,13 +23,25 @@
       </div>
       <button onclick="logout()" id="sairdomenuperfil" class="sairP"><a href="login.html" style="color: red;">Sair</a></button>
       </div>
-`;
+`
 
-     } // else {
-    //   window.location.href = "/index.html"; // se não estiver logado
-    // }
+  document.getElementById("fotodeperfiluser").innerHTML = `<img src=${usuario.foto} alt="logo do usuário">`
+  document.getElementById("infoUserContratar").innerHTML =`
+  <h1>${usuario.nome}</h1>
+                        <p>@${usuario.nome_completo}</p>
+                        <p class="cidadeepais"> <span class="UsoCP">cidade e país</span></p>
+                        <div class="seguirUser2">
+                            <button class="botaoSelecionadoUser" id="editarUserPerfil"> <span class="UsoE">Editar
+                                </span></button>
+                        </div>
+                        <button id="trocarUserPerfil"> <span class="UsoTU">Trocar de Usuário</span></button>
+  `
+;
+} // else {
+//   window.location.href = "/index.html"; // se não estiver logado
+// }
 
-    function logout() {
-      localStorage.removeItem("usuarioLogado");
-      window.location.href = "/login.html";
-    }
+function logout() {
+  localStorage.removeItem("usuarioLogado");
+  window.location.href = "/login.html";
+}
