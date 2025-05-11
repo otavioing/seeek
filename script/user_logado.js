8e03819301065d3a4431d235e41092b45b4551e7
otavioing <otaviodominguessilva@gmail.com>
2025-05-11 17:45:21 -0300

    localStorage não tem mais os dados do usuario

--- script/user_logado.js
+++ script/user_logado.js
@@ -1,36 +1,35 @@
 const usuario = JSON.parse(localStorage.getItem("usuarioLogado"));
 
-if (usuario) {
-  document.getElementById(
-    "usersite"
-  ).innerHTML = `<p id="nome_user">${usuario.nome}</p>
-      <button onclick="abrirMenu()">
-        <img id="img_user" src="http://localhost:4500${usuario.foto}" alt="User" />
-      </button>`;
-      document.getElementById("divmeuperfil").innerHTML = `<div class="divmenuperfil">
-        <a href="http://localhost:4500/usuario.html?id=${usuario.id}" class="Perf">Meu Perfil</a>
-      </div>`;
-      document.getElementById("divconfiguracoes").innerHTML = `        <a href="config.html" class="Conf">Configurações</a>
-`;
-document.getElementById("diventrarsair").innerHTML = `      <button onclick="logout()" id="sairdomenuperfil" class="sairP"> <a class="sairP" href="login.html">Sair</a></button>
-`;
+
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const response = await fetch(`http://localhost:4500/usuarios/${usuario.id}`);
+    const data = await response.json();
+
+    if (Array.isArray(data) && data.length === 1) {
+      console.log("Usuário encontrado:", data[0]);
+      //header
+      document.getElementById("nome_user").innerHTML = data[0].nome;
+      document.getElementById("img_user").src = data[0].foto;
+      document.getElementById("img_useres").src = data[0].foto;
+      // fim do header
+      // menu perfil
+      document.getElementById("divmeuperfil").innerHTML = `<div class="divmenuperfil"><a href="http://localhost:4500/usuario.html?id=${usuario.id}" class="Perf">Meu Perfil</a></div>`;
+      document.getElementById("divconfiguracoes").innerHTML = `<a href="config.html" class="Conf">Configurações</a>`;
+      document.getElementById("diventrarsair").innerHTML = `<button onclick="logout()" id="sairdomenuperfil" class="sairP"> <a class="sairP" href="login.html">Sair</a></button>`;
+      // fim do menu perfil
+    } else {
+     console.log("Usuário não encontrado ou dados inválidos.");
 
 
-  document.getElementById("fotodeperfiluser").innerHTML = `<img src=${usuario.foto} alt="logo do usuário">`
-  document.getElementById("infoUserContratar").innerHTML =`
-  <h1>${usuario.nome}</h1>
-                        <p>@${usuario.nome_completo}</p>
-                        <p class="cidadeepais"> <span class="UsoCP">cidade e país</span></p>
-                        <div class="seguirUser2">
-                            <button class="botaoSelecionadoUser" id="editarUserPerfil"> <span class="UsoE">Editar
-                                </span></button>
-                        </div>
-                        <button id="trocarUserPerfil"> <span class="UsoTU">Trocar de Usuário</span></button>
-  `
-;
-} // else {
-//   window.location.href = "/index.html"; // se não estiver logado
-// }
+    }
+  } catch (error) {
+  console.log("Erro ao buscar dados do usuário:", error);
+  alert("Você não está logado", error);
+}
+
+});
+
 
 if (!usuario.acessibilidade_ativa) {
   document.getElementById('barra-acessibilidade').style.display = 'none';
