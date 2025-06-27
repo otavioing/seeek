52a39367f46eb596c6c0bf1e4ed3b48d90ac8f24
MiguelGalhardoLima <miguelgalhardo76@gmail.com>
2025-06-27 10:16:07 -0300

    Resumo
    ° Melhorei a padronização de cursos e vagas
    ° alterei a função de dar zoom da barra de acessibilidade, agora funciona certinho
    ° fiz com que o modal puxasse o nome e a foto do usuário que postou e garanti que o post fosse dele também para não quebrar
    ° adicionei novas imagens

--- script/acessibilidade.js
+++ script/acessibilidade.js
@@ -1,9 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const usuario = JSON.parse(localStorage.getItem("usuarioLogado"));
-    const barra = document.getElementById('barra-acessibilidade');
-  
-    if (usuario && barra && typeof usuario.acessibilidade_ativa !== 'undefined') {
-      barra.style.display = usuario.acessibilidade_ativa ? 'block' : 'none';
+  const usuario = JSON.parse(localStorage.getItem("usuarioLogado"));
+  const barra = document.getElementById('barra-acessibilidade');
+
+  if (usuario && barra && typeof usuario.acessibilidade_ativa !== 'undefined') {
+    barra.style.display = usuario.acessibilidade_ativa ? 'block' : 'none';
+  }
+
+  // Configuração de fonte
+  window.fonteAtual = 100; // 100% = padrão
+  const maxFonte = 150;
+  const minFonte = 80;
+
+  window.zoommais = function () {
+    if (fonteAtual < maxFonte) {
+      fonteAtual += 10;
+      aplicarZoomFonte();
     }
-  });
-  
+  };
+
+  window.zoommenos = function () {
+    if (fonteAtual > minFonte) {
+      fonteAtual -= 10;
+      aplicarZoomFonte();
+    }
+  };
+
+  function aplicarZoomFonte() {
+    document.documentElement.style.fontSize = `${fonteAtual}%`;
+  }
+});
