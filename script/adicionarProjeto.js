9e42dd11302d181aa1b9c38843b662729254c1e6
otavioing <162048765+otavioing@users.noreply.github.com>
2025-11-09 16:51:21 -0300

    Add category selection to project submission form
    Introduces a radio button group for category selection in the project submission form and updates the JavaScript to include the selected category when submitting a new project. Also improves the styling for category selection and adjusts padding and layout for better usability.

--- script/adicionarProjeto.js
+++ script/adicionarProjeto.js
@@ -7,12 +7,15 @@ form.addEventListener('submit', async (e) => {
   const descricao = document.getElementById('text-new-post').value;
   const titulo = document.getElementById('textoTituloProjeto').value;
   const user_id = usuario.id;
+  const categoria = document.querySelector('input[name="categoria"]:checked')?.value;
+  alert(categoria); 
 
   const formData = new FormData();
   formData.append("arquivo", arquivo);
   formData.append("legenda", descricao);
   formData.append("titulo", titulo);
   formData.append("user_id", user_id);
+  formData.append("categoria", categoria);
 
   try {
     const response = await fetch("http://localhost:4500/posts", {
@@ -36,20 +39,20 @@ form.addEventListener('submit', async (e) => {
 });
 
 
-    function abrirnotificacaopostenviado(){
+function abrirnotificacaopostenviado() {
   let notificacao = document.getElementById("caixadenotificacaopostrealizadocomsucesso");
 
   notificacao.style.top = "0rem";
-      setTimeout(() => {
-        notificacao.style.top = "-10rem";
-    }, 3000);
+  setTimeout(() => {
+    notificacao.style.top = "-10rem";
+  }, 3000);
 }
 // <div id="caixadenotificacaopostrealizadocomsucesso" class="caixadenotificacaorealizadocomsucesso">
 //  <h2>Post enviado com sucesso!</h2>
 //  <p>Seu conteúdo foi publicado e já pode ser visualizado por outros usuários.</p>
 // </div>
 
- function autoResize(textarea) {
- textarea.style.height = "auto"; // reseta a altura
+function autoResize(textarea) {
+  textarea.style.height = "auto"; // reseta a altura
   textarea.style.height = textarea.scrollHeight + "px"; // ajusta para o conteúdo
 }
